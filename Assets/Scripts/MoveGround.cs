bf17ffa2fd6cecbf3bc887889d0399576dc4fcc0
Chad Linthicum <portableanswers@gmail.com>
2023-07-23 15:01:40 -0400

    Refactored ground movement script.

--- Assets/Scripts/MoveGround.cs
+++ Assets/Scripts/MoveGround.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class MoveBackwards : MonoBehaviour
 {
     public float speed;
-    private PlayerController playerControllerScript;
+    private Vector3 startPos;
 
     void Start()
     {
-
+        startPos = transform.position;
     }
-
     void Update()
     {
         transform.Translate(Vector3.back * Time.deltaTime * speed);
+        if (transform.position.z < -400)
+        {
+            transform.position = startPos;
+
+        }
     }
 }
