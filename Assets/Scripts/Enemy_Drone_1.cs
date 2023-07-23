b7bb698a51d52ef3e1d0d918a28d4d4034552c01
Chad Linthicum <portableanswers@gmail.com>
2023-07-23 15:55:38 -0400

    Enemy moves and stops in preparation to shoot a projectile at the player.

--- Assets/Scripts/Enemy_Drone_1.cs
+++ Assets/Scripts/Enemy_Drone_1.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Enemy_Drone_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public float speed = 150.0f;
+    //public float zThreshold = 92.0f;
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(transform.localPosition.z);
 
+        if (transform.localPosition.z > -815.0f)
+        {
+            Move();
+        }
+        else
+        {
+            Invoke("Move", 1.0f);
+        }
+        if (transform.localPosition.z < -1200.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Move()
+    {
+        transform.position += Vector3.back * speed * Time.deltaTime;
     }
 }
