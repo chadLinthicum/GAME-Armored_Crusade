6fe08439b9480c05de429b05fd85dc4f4eab6834
Chad Linthicum <portableanswers@gmail.com>
2023-07-24 19:20:17 -0400

    Added lighting to Mechoid.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,17 +6,17 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] mechoidPrefab;
 
-    public float spawnY = 100f;
-    public float spawnZ = 872f;
+    public float spawnY = 92.5f;
+    public float spawnZ = 1100f;
 
     public Vector3[] positions;
 
     void Start()
     {
         positions = new Vector3[] {
-            new Vector3(-175.4f, spawnY, spawnZ),
-            new Vector3(-27.7f, spawnY, spawnZ),
-            new Vector3(125.35f, spawnY, spawnZ)
+            new Vector3(-115f, spawnY, spawnZ),
+            new Vector3(-15f, spawnY, spawnZ),
+            new Vector3(85f, spawnY, spawnZ)
         };
         InvokeRepeating("spawnMechoid", 0f, 3f);
     }
