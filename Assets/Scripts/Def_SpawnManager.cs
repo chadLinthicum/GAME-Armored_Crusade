68fc8e0e3d469d37b1ade4a8416e8bcdf0cac73d
Chad Linthicum <portableanswers@gmail.com>
2023-07-24 15:12:27 -0400

    New model for Mechoid enemy.

---
+++ Assets/Scripts/Def_SpawnManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] mechoidPrefab;
+
+    float spawnY = 106.3f;
+    float spawnZ = 872f;
+
+    public Vector3[] positions;
+
+    void Start()
+    {
+        positions = new Vector3[] {
+            new Vector3(-175.4f, spawnY, spawnZ),
+            new Vector3(-27.7f, spawnY, spawnZ),
+            new Vector3(125.35f, spawnY, spawnZ)
+        };
+        InvokeRepeating("spawnMechoid", 0f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int mechoidIndex = Random.Range(0, positions.Length);
+            Vector3 randomPosition = positions[mechoidIndex];
+            Instantiate(mechoidPrefab[0], randomPosition, mechoidPrefab[0].transform.rotation);
+        }
+    }
+
+    void spawnMechoid()
+    {
+        int mechoidIndex = Random.Range(0, positions.Length);
+        Vector3 randomPosition = positions[mechoidIndex];
+        Instantiate(mechoidPrefab[0], randomPosition, mechoidPrefab[0].transform.rotation);
+    }
+}
