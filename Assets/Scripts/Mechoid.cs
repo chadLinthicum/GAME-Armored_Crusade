841742590fecd825d55c154cd79d712caccfcc75
Chad Linthicum <portableanswers@gmail.com>
2023-07-28 19:28:43 -0400

    Mechoid has separate move and idle animations.

--- Assets/Scripts/Mechoid.cs
+++ Assets/Scripts/Mechoid.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Drone_1 : MonoBehaviour
+public class Mechoid : MonoBehaviour
 {
     public float speed = 150.0f;
-    public float zThreshold = 90.0f;
+    public float zThreshold = 100.0f;
     public float backBound = -1200.0f;
+    private Animator childAnimator;
+    private bool fired = false;
+
+    void Start()
+    {
+        childAnimator = GetComponentInChildren<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,18 +22,38 @@ public class Enemy_Drone_1 : MonoBehaviour
         {
             Move();
         }
-        else
+        if (transform.position.z < zThreshold && fired == false)
         {
-            Invoke("Move", 1.0f);
+            childAnimator.SetBool("isMoving", false);
+
+            //TODO - Code for firing attack
+
+            Invoke("StartMovingAgain", 2.0f);
+
         }
-        if (transform.localPosition.z < backBound)
+        if (fired == true)
         {
-            Destroy(gameObject);
+            Move();
         }
+
+        CheckBounds();
     }
 
     void Move()
     {
         transform.position += Vector3.back * speed * Time.deltaTime;
+        childAnimator.SetBool("isMoving", true);
+    }
+    void StartMovingAgain()
+    {
+        childAnimator.SetBool("isMoving", true);
+        fired = true;
+    }
+    void CheckBounds()
+    {
+        if (transform.localPosition.z < backBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
