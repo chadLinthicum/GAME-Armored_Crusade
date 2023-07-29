b24517f5b13330e95c80f65c793ea9c723cbe147
Johan Östling <johan.f.ostling@gmail.com>
2023-07-29 15:02:19 +0200

    Add smooth stop/start for drones

--- Assets/Scripts/Mechoid.cs
+++ Assets/Scripts/Mechoid.cs
@@ -7,6 +7,15 @@ public class Mechoid : MonoBehaviour
     public float speed = 150.0f;
     public float zStop = 100.0f;
     public float boundary = -1200.0f;
+
+    [Tooltip("The distance from zStop that the drone starts decelerating / stops accelerating")]
+    public float accelerationDistance = 10f;
+    [Tooltip("Minimum speed during acceleration and deceleration, needs to be > 0 to avoid problem reaching and leaving the stop point")]
+    [Min(0.1f)]
+    public float minimumSpeed = 1.5f;
+    [Tooltip("The curve of speed over distance from stop point")]
+    public AnimationCurve accelerationCurve;
+    
     private bool fired = false;
     private Animator childAnimator;
 
@@ -41,7 +50,15 @@ public class Mechoid : MonoBehaviour
 
     void Move()
     {
-        transform.position += Vector3.back * speed * Time.deltaTime;
+        var actualSpeed = speed;
+        var positionZ = transform.position.z;
+        var offset = Mathf.Abs(positionZ - zStop);
+        if (offset < accelerationDistance)
+        {
+            var positionOnDecelerationPath = offset / accelerationDistance;
+            actualSpeed = Mathf.Lerp(minimumSpeed, speed,accelerationCurve.Evaluate(positionOnDecelerationPath));
+        }
+        transform.position += Vector3.back * actualSpeed * Time.deltaTime;
         childAnimator.SetBool("isMoving", true);
     }
     void StartMovingAgain()
