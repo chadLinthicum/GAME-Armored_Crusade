0adc1cc86083ce6e28d818deed5ff1e0037bc21f
Chad Linthicum <portableanswers@gmail.com>
2023-08-11 18:09:20 -0400

    Adjust UI, Subbed UI health for health indicator on player tank, adjusted player bounds.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,21 +8,22 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
     public float speed = 30.0f;
-    //private float xRange = 16.0f;
-    //private float zRangeBottom = -4.0f;
-    //private float zRangeTop = 16.0f;
+    public float xRangeLeft = -16.0f;
+    public float xRangeRight = 16.0f;
+    public float zRangeBottom = -4.0f;
+    private float zRangeTop = 16.0f;
 
     public GameObject projectilePrefab;
 
     // GUI components we can update
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI timerText;
-    public TextMeshProUGUI healthText;
+    public TextMeshPro healthText;
 
     // data shown on the GUI
     private int playerScore = 0;
     private float playerTime = 0;
-    private float playerHealth = 100;
+    private float playerHealth = 60;
 
     // Start is called before the first frame update
     void Start()
@@ -30,31 +31,35 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    string formatTime(float timeInSeconds) {
+    string formatTime(float timeInSeconds)
+    {
         int timeInSecondsInt = (int)timeInSeconds;
-        int minutes = (int) (timeInSeconds / 60f);
+        int minutes = (int)(timeInSeconds / 60f);
         int seconds = timeInSecondsInt - (minutes * 60);
         return minutes.ToString("D2") + ":" + seconds.ToString("D2");
     }
 
-    string formatHealth(float hp) {
+    string formatHealth(float hp)
+    {
         string healthStr = "";
         int numBars = Mathf.RoundToInt(hp / 10f);
-        for (int i=0; i<numBars; i++) {
+        for (int i = 0; i < numBars; i++)
+        {
             healthStr += "█";
         }
-        return healthStr;   
+        return healthStr;
     }
 
-    void updateGUI() {
-        
-        if (scoreText) scoreText.text = "Score:\n" + playerScore.ToString("000000");
+    void updateGUI()
+    {
 
-        if (timerText) timerText.text = "Time:\n" + formatTime(playerTime);
-        
-        if (healthText) healthText.text = "Health: " + formatHealth(playerHealth);
+        if (scoreText) scoreText.text = "SCORE\n" + playerScore.ToString("000000");
+
+        if (timerText) timerText.text = "TIMER\n" + formatTime(playerTime);
+
+        if (healthText) healthText.text = formatHealth(playerHealth);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -62,42 +67,33 @@ public class PlayerController : MonoBehaviour
         playerScore++;
 
         // increase player time
-        playerTime += Time.deltaTime; 
-        
-        // Move object horizontally based on mouse movement
+        playerTime += Time.deltaTime;
 
-        // Keep the player in bounds
-        //if (transform.position.x < -xRange)
-        //{
-        //    transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        //}
-        //if (transform.position.x > xRange)
-        //{
-        //    transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        //}
+        //Keep play in bounds
+        if (transform.position.x < -xRangeLeft)
+        {
+            transform.position = new Vector3(-xRangeLeft, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRangeRight)
+        {
+            transform.position = new Vector3(xRangeRight, transform.position.y, transform.position.z);
+        }
 
-        //if (transform.position.z < zRangeBottom)
-        //{
-        //    transform.position = new Vector3(transform.position.x, transform.position.y, zRangeBottom);
-        //}
-        //if (transform.position.z > zRangeTop)
-        //{
-        //    transform.position = new Vector3(transform.position.x, transform.position.y, zRangeTop);
-        //}
+        if (transform.position.z < zRangeBottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeBottom);
+        }
+        if (transform.position.z > zRangeTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeTop);
+        }
 
         // Player input
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(transform.right * horizontalInput * Time.deltaTime * speed);
 
-        //verticalInput = Input.GetAxis("Vertical");
-        //transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
-
-
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        //}
-
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
         updateGUI();
     }
 }
