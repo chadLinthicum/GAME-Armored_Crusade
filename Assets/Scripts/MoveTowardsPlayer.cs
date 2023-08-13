acf618113a87518f533a6eba5569e04f2eeac1a5
Chad Linthicum <portableanswers@gmail.com>
2023-08-13 12:34:36 -0400

    Projectile now moves only to player last position when fired. However it's not yet accurate.

--- Assets/Scripts/MoveTowardsPlayer.cs
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -6,18 +6,20 @@ public class MoveTowardsPlayer : MonoBehaviour
 {
     private GameObject player;
     public float speed;
+    public Vector3 playerPos;
 
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        playerPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
         Destroy(gameObject, 5f);
     }
 }
