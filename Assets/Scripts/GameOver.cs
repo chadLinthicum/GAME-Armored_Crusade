12cbd007b67430152a1e8703ace1675d28d5d20c
Chad Linthicum <chad.linthicum@saic.com>
2023-08-17 16:08:44 -0400

    Simple Game Over screen created. Home button will eventually go to Start Screen. Mouse pointer not working, due to cursor lock most likely.

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MyButtonScript : MonoBehaviour
+{
+    public void btn_Home(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void btn_Retry(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+}
