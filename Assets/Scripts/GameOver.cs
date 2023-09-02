6c67fd16c4ca1c587177d962cb9997aab3774b81
Chad Linthicum <portableanswers@gmail.com>
2023-09-02 19:23:32 -0400

    Fixed bug of waves not resetting upon game over.

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -6,6 +6,10 @@ public class MyButtonScript : MonoBehaviour
 {
     public void btn_Home(int sceneID)
     {
+        Score.playerScore = 0;
+        SpawnManager.wave2 = false;
+        SpawnManager.wave3 = false;
+        SpawnManager.wave4 = false;
         Time.timeScale = 1f;
         SceneManager.LoadScene(sceneID);
     }
@@ -13,6 +17,10 @@ public class MyButtonScript : MonoBehaviour
     public void btn_Retry(int sceneID)
     {
         Score.playerScore = 0;
+        SpawnManager.wave2 = false;
+        SpawnManager.wave3 = false;
+        SpawnManager.wave4 = false;
+        SpawnManager.boss = false;
         Time.timeScale = 1f;
         SceneManager.LoadScene(sceneID);
     }
